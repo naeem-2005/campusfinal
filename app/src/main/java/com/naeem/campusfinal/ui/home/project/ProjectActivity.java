a3cbe0c2d068aaa37c6c73ae85777bceed1ad64a
naeem-2005 <naeemkhan7d@gmail.com>
2023-03-01 23:52:52 +0530

    this is search page 50%

---
+++ app/src/main/java/com/naeem/campusfinal/ui/home/project/ProjectActivity.java
@@ -0,0 +1,16 @@
+package com.naeem.campusfinal.ui.home.project;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.naeem.campusfinal.R;
+
+public class ProjectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_project);
+    }
+}
