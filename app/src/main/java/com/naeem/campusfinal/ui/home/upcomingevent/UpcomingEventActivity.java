dc87134ae9d0f0f8ba98e2946c4e85ad8c5f4aae
naeem-2005 <naeemkhan7d@gmail.com>
2023-03-01 22:08:10 +0530

    this is my campusapp2

---
+++ app/src/main/java/com/naeem/campusfinal/ui/home/upcomingevent/UpcomingEventActivity.java
@@ -0,0 +1,16 @@
+package com.naeem.campusfinal.ui.home.upcomingevent;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.naeem.campusfinal.R;
+
+public class UpcomingEventActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_upcoming_event);
+    }
+}
