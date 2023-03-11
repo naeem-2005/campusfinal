b91ad7db0c131f40a8074bca438a7a69fed7ac74
naeem-2005 <naeemkhan7d@gmail.com>
2023-03-11 11:53:51 +0530

    Firebase database connected

--- app/src/main/java/com/naeem/campusfinal/MainActivity.java
+++ app/src/main/java/com/naeem/campusfinal/MainActivity.java
@@ -34,119 +34,38 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
     private NavigationView navigationView;
     private DrawerLayout drawerLayout ;
     private ActionBarDrawerToggle toggle;
-    private NavigationView navigationView2;
+    private NavController navController;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-         drawerLayout=findViewById(R.id.drawer_layout);
-         navigationView2=findViewById(R.id.dashboard_item);
-         toggle=new ActionBarDrawerToggle(this,drawerLayout,R.string.start,R.string.close);
-         drawerLayout.addDrawerListener(toggle);
-         toggle.syncState();
-         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
 
-         navigationView2.setNavigationItemSelectedListener(this);
+        bottomNavigationView = findViewById(R.id.bottomNavigationView);
+        navController = Navigation.findNavController(this, R.id.frame_layout);
+        drawerLayout = findViewById(R.id.drawer_layout);
+        navigationView = findViewById(R.id.dashboard_item);
 
-        bottomNavigationView=findViewById(R.id.bottomNavigation);
+        NavigationUI.setupWithNavController(bottomNavigationView, navController);
+        toggle = new ActionBarDrawerToggle(this, drawerLayout, R.string.start, R.string.close);
+        drawerLayout.addDrawerListener(toggle);
+        toggle.syncState();
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-                int id=item.getItemId();
-                if (id==R.id.navigation_home)
-                {
-                   loadFrag(new HomeFragment(),false);
-                }
-                else if(id==R.id.navigation_search)
-                {
-                    loadFrag(new SearchFragment(),false);
-                }
-                else if (id==R.id.navigation_activity)
-                {
-                    loadFrag(new ActivityFragment(),false);
-                }
-                else if(id==R.id.navigation_profile)
-                {
-                    loadFrag(new ProfileFragment(),false);
-                }
-                else
-                {
-                    loadFrag(new NewProjectFragment(),false);
-                }
-                return true;
-
-            }
-
-
-        });
-
-        bottomNavigationView.setSelectedItemId(R.id.navigation_home);
+        navigationView.setNavigationItemSelectedListener(this);
 
     }
-
-    public void loadFrag(Fragment fragment,boolean flag){
-
-        FragmentManager fragmentManager=getSupportFragmentManager();
-        FragmentTransaction ft=fragmentManager.beginTransaction();
-
-        if (flag) {
-
-            ft.add(R.id.frame_layout, fragment);
-//            String ROOT_FRAGMENT_TAG = null;
-//            fragmentManager.popBackStack(ROOT_FRAGMENT_TAG, fragmentManager.POP_BACK_STACK_INCLUSIVE);
-//            ft.addToBackStack(ROOT_FRAGMENT_TAG);
-        }
-       else {
-            ft.replace(R.id.frame_layout, fragment);
-//            ft.addToBackStack(null);
-        }
-        ft.commit();
-
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if(toggle.onOptionsItemSelected(item)){
-            return true;
-        }
-
-        return true;
-    }
-
-    @Override
-    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-
-        switch (item.getItemId())
-        {
-            case R.id.dashboard_share:
-                Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.dashboard_rate_us:
-                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.dashboard_about_us:
-                Toast.makeText(this, "newproject", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.dashboard_privacy:
-                Toast.makeText(this, "activity", Toast.LENGTH_SHORT).show();
-                break;
-            case R.id.dashboard_term:
-                Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
-                break;
-        }
-        return true;
-    }
-
     @Override
     public void onBackPressed() {
 
 
-        if (bottomNavigationView.getSelectedItemId() == R.id.navigation_home){
+        if (bottomNavigationView.getSelectedItemId() == R.id.navigation_home) {
 
             AlertDialog.Builder builder = new AlertDialog.Builder(this);
             builder.setMessage("Do you want to exit?");
@@ -168,10 +87,40 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
 
             AlertDialog dialog = builder.create();
             dialog.show();
-        }
-        else {
+        } else {
             bottomNavigationView.setSelectedItemId(R.id.navigation_home);
         }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (toggle.onOptionsItemSelected(item)) {
+            return true;
+        }
+
+        return true;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.dashboard_share:
+                Toast.makeText(this, "home", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dashboard_rate_us:
+                Toast.makeText(this, "search", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dashboard_about_us:
+                Toast.makeText(this, "newproject", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dashboard_privacy:
+                Toast.makeText(this, "activity", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.dashboard_term:
+                Toast.makeText(this, "profile", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return true;
     }
 
 }
